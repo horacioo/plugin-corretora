3544519bad7fb6afdf7a04112eb6f81936c7d4c7
horacio <lanterna_@hotmail.com>
2017-11-26 21:17:45 -0200

    26-11-2017

--- classes/formulario.php
+++ classes/formulario.php
@@ -5,7 +5,8 @@ class Formulario
     static $campos=[
         'nome',
         'cpf',
-        'email'
+        'email',
+        'profissao'
         ];
 
     
@@ -13,6 +14,5 @@ class Formulario
     static public function nome(){return "<p><label>Nome:</label><input type='text' required='required' name='nome' id='nomeForm' ></p>";}
     static public function cpf(){return "<p><label>Cpf:</label><input type='text' required='required' name='cpf' id=\"cpfForm\" ></p>";}
     static public function email(){return "<p><label>E-mail:</label><input type='email' required='required' name='email' id='emailForm' ></p>";}
-    
-    
+    static public function profissao(){return "<p><label>profissão:</label><select name='profissao'><option value='#'>selecione</option></select></p>";}
     }
